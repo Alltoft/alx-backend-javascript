2c73cede9d79ea79fe1d3ff55a73cbef35d2fbeb
Emran <ouakouriimran@gmail.com>
2024-05-28 22:14:29 +0100

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,11 +3,11 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const User = signUpUser;
-  const photo = uploadPhoto;
   return Promise.allSettled([
-    {
-      status: Promise.status,
-    },
-  ]);
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((response) => response.map((Promise) => ({
+    status: Promise.status,
+    value: Promise.status === 'fulfilled' ? Promise.value : string(Promise.reason),
+  })));
 }
