9f9629ddb53be1931b0d50dec30c7db686d2e6a4
Emran <ouakouriimran@gmail.com>
2024-05-28 22:13:57 +0100

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,12 +1,13 @@
-import signUpUser from './4-user-promise'
-import uploadPhoto from './5-photo-reject'
+/*eslint-disable*/
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName){
-    const User = signUpUser
-    const photo = uploadPhoto
-    return Promise.allSettled([
-        {
-            status: Promise.status
-        }
-    ])
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const User = signUpUser;
+  const photo = uploadPhoto;
+  return Promise.allSettled([
+    {
+      status: Promise.status,
+    },
+  ]);
 }
