2f0613b835e16c63594f206cf0ffedae10921841
Emran <ouakouriimran@gmail.com>
2024-05-31 00:07:07 +0100

    10-car.js

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,11 +1,11 @@
 export default class car {
-    constructor(brand, motor, color) {
-        this._brand = brand;
-        this._motor = motor;
-        this._color = color;
-    }
-    cloneCar() {
-        const Species = this.constractor[Symbol.species];
-        return new Species();
-    }
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    return new constractor();
+  }
 }
