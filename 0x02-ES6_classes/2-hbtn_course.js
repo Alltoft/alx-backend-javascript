197cb9ab0cf12f732becabc3aaf703cf092c4c95
Emran <ouakouriimran@gmail.com>
2024-05-30 19:38:19 +0100

    2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -8,7 +8,7 @@ export default class HolbertonCourse {
       throw new TypeError('Length must be a number');
     }
     if ((!Array.isArray(students) || !students.every((student) => typeof student === 'string'))) {
-      throw new TypeError('students must be an array of Strings');
+      throw new TypeError('Students must be an array of Strings');
     }
 
     this._name = name;
@@ -21,6 +21,9 @@ export default class HolbertonCourse {
   }
 
   set name(newname) {
+    if (typeof newname !== 'string') {
+        throw new TypeError('Name must be a string');
+      }
     return this._name = newname;
   }
 
@@ -29,6 +32,9 @@ export default class HolbertonCourse {
   }
 
   set length(newlength) {
+    if (typeof newlength !== 'number') {
+        throw new TypeError('Length must be a number');
+      }
     return this._length = newlength;
   }
 
@@ -37,6 +43,9 @@ export default class HolbertonCourse {
   }
 
   set students(newstudents) {
+    if (!Array.isArray(value) || !value.every((student) => typeof student === 'string')) {
+        throw new TypeError('Students must be an array of strings');
+    }
     return this._students = newstudents;
   }
 }
