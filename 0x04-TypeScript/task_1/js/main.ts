4a1bf3287179a49d2749f695cd6b640851bcff3e
Emran <ouakouriimran@gmail.com>
2024-06-05 21:17:32 +0100

    all tasks

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,25 @@
+class StudentClass {
+    constructor(firstName: string, lastName: string) {
+    }
+}
+
+interface Teacher {
+    readonly firstName: string,
+    readonly lastName: string,
+    fullTimeEmployee: boolean,
+    yearsOfExperience?: number,
+    location: string,
+    contract: boolean,
+};
+
+interface Directors extends Teacher {
+    numberOfReports: number,
+}
+
+interface  printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+	return `${firstName[0]}. ${lastName}`;
+};
