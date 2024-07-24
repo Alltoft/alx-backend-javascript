ca062dabc3ec8f56e954d0090a227ab2a67e6e26
Emran <ouakouriimran@gmail.com>
2024-07-24 20:52:57 +0100

    3-payment.

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,17 @@
+const Utils = {
+  calculateNumber(type, a, b) {
+    const Ra = Math.round(a);
+    const Rb = Math.round(b);
+    if (type == 'SUM')
+        return Ra + Rb;
+    else if (type == 'SUBTRACT')
+        return Ra - Rb
+    else if (type == 'DIVIDE')
+        if (Rb == 0)
+            return "Error"
+        else
+            return Ra / Rb
+  }
+};
+  
+module.exports = Utils;
