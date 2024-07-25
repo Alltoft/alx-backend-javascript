32f2ae75995a8a62deff74cc6a3e5d80d6e333dd
Emran <ouakouriimran@gmail.com>
2024-07-26 00:10:51 +0100

    10-api/

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -1,69 +1,80 @@
 const request = require('request');
-const { expect } = require('chai');
+const chai = require('chai');
 
-describe('Index page', function () {
-  it('should have the correct status code', function (done) {
-    request('http://localhost:7865', function (error, response) {
-      expect(response.statusCode).to.equal(200);
-      done();
-    });
-  });
+const { expect } = chai;
 
-  it('should have the correct result', function (done) {
-    request('http://localhost:7865', function (error, response, body) {
-      expect(body).to.equal('Welcome to the payment system');
-      done();
-    });
-  });
-
-  it('other?', function (done) {
-    request('http://localhost:7865', function (error, response, body) {
-      expect(body).to.not.equal('Something else');
-      done();
-    });
-  });
-
-  it('should have the correct status code when :id is a number?', function (done) {
-    request('http://localhost:7865/cart/12', function (error, response, body) {
-      expect(response.statusCode).to.equal(200);
-      expect(body).to.equal('Payment methods for cart 12');
-      done();
-    });
-  });
-
-  it('should have the correct status code when :id is NOT a number (=> 404)?', function (done) {
-    request('http://localhost:7865/cart/hello', function (error, response) {
-      expect(response.statusCode).to.equal(404);
-      done();
-    });
-  });
-
-  describe('/login', function () {
-    it('should have the user login', function (done) {
-      const options = {
-        method: 'POST',
-        url: 'http://localhost:7865/login',
-        json: { userName: 'Betty' }
-      };
-
-      request(options, function (error, response, body) {
-        expect(body).to.equal('Welcome Betty');
-	done();
+describe('Index page', () => {
+  it('response', (done) => {
+    try {
+      request('http://localhost:7865/', (error, response, body) => {
+        if (error) throw error;
+        expect(body).to.equal('Welcome to the payment system');
+        expect(response.statusCode).to.equal(200);
+        done();
       });
+    } catch (error) {
+      done(error);
+    }
+  });
+  describe('cart', () => {
+    it('response', (done) => {
+      try {
+        request('http://localhost:7865/cart/12', (error, response, body) => {
+          if (error) throw error;
+          expect(body).to.equal('Payment methods for cart 12');
+          expect(response.statusCode).to.equal(200);
+          done();
+        });
+      } catch (error) {
+        done(error);
+      }
+    });
+    it('not number', (done) => {
+      try {
+        request('http://localhost:7865/cart/uber', (error, response) => {
+          if (error) throw error;
+          expect(response.statusCode).to.equal(404);
+          done();
+        });
+      } catch (error) {
+        done(error);
+      }
     });
   });
-
-  describe('/available_payments', function () {
-    it('should have the available payment methods', function (done) {
-      const options = {
-	payment_methods: {
-          credit_cards: true,
-          paypal: false
-	}
-      };
-
-      expect(JSON.parse(body)).to.deep.equal(expectedResponse);
-      done();
+  describe('available_payments', () => {
+    it('response', (done) => {
+      try {
+        request('http://localhost:7865/available_payments', (error, response, body) => {
+          if (error) throw error;
+          expect(JSON.parse(body)).to.deep.equal({
+            payment_methods: {
+              credit_cards: true,
+              paypal: false,
+            },
+          });
+          expect(response.statusCode).to.equal(200);
+          done();
+        });
+      } catch (error) {
+        done(error);
+      }
+    });
+  });
+  describe('login', () => {
+    it('response', (done) => {
+      try {
+        request.post({
+          url: 'http://localhost:7865/login',
+          json: { userName: 'Betty' },
+        }, (error, response, body) => {
+          if (error) throw error;
+          expect(body).to.equal('Welcome Betty');
+          expect(response.statusCode).to.equal(200);
+          done();
+        });
+      } catch (error) {
+        done(error);
+      }
     });
   });
 });
